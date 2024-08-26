05faddbd46f87545d722e135382e7dbe0f7207c2
Fiona0229 <fiona2016@protonmail.com>
2024-08-26 02:08:50 -0700

    Reduce commented code.

--- ao3-visualizer/src/utils/dataGetter.js
+++ ao3-visualizer/src/utils/dataGetter.js
@@ -1,26 +1,3 @@
-// function getPages() {
-//     const pages =
-//         document.querySelector(
-//             "ol.pagination.actions"
-//         );
-
-//     if (!pages) {
-//         return 1;
-//     }
-
-//     const listItems =
-//         pages.querySelectorAll("li");
-//     const lastPageItem =
-//         listItems[listItems.length - 2];
-//     const lastPageLink =
-//         lastPageItem.querySelector("a");
-
-//     return lastPageLink.textContent;
-// }
-
-// // Example usage
-// console.log(getPages());
-
 import { JSDOM } from "jsdom";
 
 async function getPages(url) {
@@ -47,8 +24,6 @@ async function getPages(url) {
             workPage.querySelector(
                 "ol.pagination.actions"
             );
-
-        console.log(pages);
 
         if (!pages) {
             return 1;
@@ -79,7 +54,7 @@ async function getPages(url) {
 
 // Example usage
 const url =
-    "https://archiveofourown.org/users/Drag0nst0rm/works"; // Replace with your URL
+    "https://archiveofourown.org/users/Ilsistemaperiodico/works"; // Replace with your URL
 getPages(url).then((lastPage) => {
     console.log("Last page:", lastPage);
 });
