0b06919716aaef3b9d26600704b134f845100ac4
Fiona0229 <fiona2016@protonmail.com>
2024-09-13 00:27:54 -0700

    Filter works to calculate in boxplot.

--- ao3-visualizer/src/components/Boxplot.jsx
+++ ao3-visualizer/src/components/Boxplot.jsx
@@ -1,15 +1,45 @@
 import React from "react";
+import { reduceToFandoms } from "../utils/conversion";
 
 const Boxplot = (workList) => {
     const works =
         Object.values(workList)[0];
 
-    const kudos = works.map(
-        (work) => work[3]
+    const fandoms =
+        reduceToFandoms(works);
+
+    const fandomsDesc = Object.entries(
+        fandoms
+    ).sort(
+        ([, valueA], [, valueB]) =>
+            valueB - valueA
     );
 
-    console.log(kudos);
-    return <div>{kudos}</div>;
+    const boxplotFandom = Object.values(
+        fandomsDesc
+            .slice(0, 5)
+            .map(([key]) => key)
+    );
+
+    const checkFandom = (work) => {
+        const fandoms = work[2].flat();
+        let set1 = new Set(fandoms);
+        let set2 = new Set(
+            boxplotFandom
+        );
+
+        return (
+            [...set1].filter((value) =>
+                set2.has(value)
+            ).length > 0
+        );
+    };
+
+    const boxplotWorks = works.filter(
+        checkFandom
+    );
+
+    return <div>Boxplot</div>;
 };
 
 export default Boxplot;
